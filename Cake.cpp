dfcd575b4bedbfe9cb3b5917ce601a478f885e81
이윤주 <josie116@naver.com>
2021-10-17 23:46:02 +0900

    Cake 수정

--- Cake.cpp
+++ Cake.cpp
@@ -15,27 +15,25 @@ void egg(ObjectPtr egg_broken, bool egg_state, int* egg_count, ObjectPtr egg_bas
 
 }
 
-void oven_complete(ObjectPtr oven, ObjectPtr cake_pre, ObjectPtr cake, ObjectPtr degree, ObjectPtr time, ObjectPtr prev, ObjectPtr next) {
+void oven_complete(ObjectPtr oven, ObjectPtr cake, ObjectPtr degree, ObjectPtr time, ObjectPtr next) {
 	showMessage("완료!");
 	oven->hide();
-	cake_pre->hide();
 	degree->hide();
 	time->hide();
 	cake->show();
-	prev->show();
 	next->show();
 
 }
 
 
 
-//문제) 비밀번호를 찾기 위해 스위치를 눌러 거실이 어두워지면, object가 상대적으로 밝아보인다.
-
 int main() {
 
 	//배경
 	auto scene1 = Scene::create("거실", "Images/거실.png");
 	auto scene2 = Scene::create("부엌", "Images/부엌.png");
+
+	auto sound = Sound::create("Audio/English Country Garden - Aaron Kenny.mp3");
 
 
 	//거실 물건
@@ -82,10 +80,9 @@ int main() {
 	auto butter_ = Object::create("Images/버터.png", scene2, 580, 260, false);
 	auto whisk = Object::create("Images/거품기.png", scene2, 600, 300, false);
 
-	auto oven = Object::create("Images/오븐.png", scene2, 200, 50, false);
-	auto degree = Object::create("Images/버튼.png", scene2, 300, 400, false);
-	auto time = Object::create("Images/버튼.png", scene2, 800, 400, false);
-	auto cake_pre = Object::create("Images/케이크 반죽.png", scene2, 390, 200, false);
+	auto oven = Object::create("Images/오븐.png", scene2, 390, 50, false);
+	auto degree = Object::create("Images/버튼.png", scene2, 500, 410, false);
+	auto time = Object::create("Images/버튼.png", scene2, 630, 410, false);
 	auto cake = Object::create("Images/케이크.png", scene2, 370, 60, false);
 	auto cream = Object::create("Images/생크림 선택.png", scene2, 1000, 500, false);
 	auto cream_choco = Object::create("Images/초코크림 선택.png", scene2, 1000, 300, false);
@@ -97,20 +94,20 @@ int main() {
 	auto deco_cherry = Object::create("Images/체리 선택.png", scene2, 1000, 500, false);
 	auto deco_candle = Object::create("Images/초 선택.png", scene2, 1000, 300, false);
 	auto deco_choco = Object::create("Images/초코 선택.png", scene2, 1000, 100, false);
-	auto deco_choco_ = Object::create("Images/초코.png", scene2, 375, 90, false);
+	auto deco_choco_ = Object::create("Images/초코.png", scene2, 370, 60, false);
 	auto deco_cream_ = Object::create("Images/크림.png", scene2, 350, 215, false);
 	auto deco_fruit = Object::create("Images/딸기.png", scene2, 600, 360, false);
 	auto deco_candle_ = Object::create("Images/초.png", scene2, 445, 300, false);
 
 	auto done = Object::create("Images/완료.png", scene2, 550, 570, false);
 
-	auto prev = Object::create("Images/이전으로.png", scene2, 50, 200, false);
 	auto next = Object::create("Images/다음으로.png", scene2, 950, 200, false);
 
 
 	auto recipe2 = Object::create("Images/레시피.png", scene2, 430, 100, false);
 
-
+	auto result= Object::create("Images/결과_성공.png", scene2, 100, 530, false);
+	auto end= Object::create("Images/끝내기.png", scene2, 1000, 300, false);
 
 	auto message_check = 0;
 	auto light_state = 1; //0:꺼짐 1: 켜짐
@@ -125,7 +122,8 @@ int main() {
 	auto chili_state = 0;
 	auto butter_state = 0;
 	//auto something = 0; //0: 선택 안 함 1: 고춧가루 선택 -1 : 넣어야 할 걸 안 넣음
-	auto oven_check = 0;
+	auto degree_check = 0;
+	auto time_check = 0;
 	auto deco_cream_state = 0;
 	auto deco_candle_state = 0;
 	auto deco_choco_state = 0;
@@ -136,14 +134,15 @@ int main() {
 	//***********************************거실***********************************//
 
 
-	showMessage("하암~ 잘 잤다!!");
+	showMessage("어? 메세지가 왔네?");
+	
 
 	phone->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
 		message->show();
 
 		if (message_check == 0) {
-			showMessage("헉! 케이크를 직접 만들어서 선물해 주기로 한 걸 까맣게 잊고 있었네!\n약속된 시간 전에 얼른 케이크를 만들어야 해!");
-			message_check = 1;
+			showMessage("헉! 케이크를 직접 만들어서 선물해 주기로 한 걸 까맣게 잊고 있었다!\n약속된 시간 전에 얼른 케이크를 만들어야 해!");
+			message_check++;
 			phone->setImage("Images/휴대폰 꺼짐.png");
 		}
 		else {
@@ -157,7 +156,12 @@ int main() {
 	message->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
 
 		message->hide();
-		return true;
+		if (message_check == 1) {
+			showMessage("흠.. 그런데 난 케이크 만드는 법을 모르는 걸? 이걸 어쩌지?");
+			message_check++;
+		}
+
+			return true;
 
 		});
 
@@ -238,7 +242,7 @@ int main() {
 		if (message_check == 0) {
 			showMessage("거실로 가서 휴대폰을 확인하자!");
 		}
-		else if (message_check == 1 && recipe1->isHanded()) {
+		else if (message_check != 0 && recipe1->isHanded()) {
 			showMessage("케이크 만들기 시작!");
 			bowl->show();
 			egg1->show();
@@ -247,6 +251,7 @@ int main() {
 			egg_basket->show();
 			recipe_wall->show();
 			to_scene1->hide();
+			sound->play(true);
 		}
 		else showMessage("아직 요리할 준비가 되지 않은 듯하다.\n 거실을 좀 더 살펴보자.");
 
@@ -418,32 +423,34 @@ int main() {
 
 	degree->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
 
-		showMessage("오븐의 온도를 맞추자");
-		showKeypad("180", degree);
-
+		if (degree_check == 0) {
+			showMessage("오븐의 온도를 맞추자");
+			showKeypad("180", degree);
+		}
 
 		return true;
 		});
 
 	degree->setOnKeypadCallback([&](ObjectPtr object)->bool {
-		oven_check++;
-		if (oven_check == 2) oven_complete(oven, cake_pre, cake, degree, time, prev, next);
+		degree_check=1;
+		if (degree_check==1&&time_check==1) oven_complete(oven, cake, degree, time, next);
 
 		return true;
 		});
 
 	time->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
 
-
-		showMessage("오븐의 시간을 맞추자");
-		showKeypad("25", time);
+		if (time_check == 0) {
+			showMessage("오븐의 시간을 맞추자");
+			showKeypad("25", time);
+		}
 
 		return true;
 		});
 
 	time->setOnKeypadCallback([&](ObjectPtr object)->bool {
-		oven_check++;
-		if (oven_check == 2) oven_complete(oven, cake_pre, cake, degree, time, prev, next);
+		time_check = 1;
+		if (degree_check == 1 && time_check == 1) oven_complete(oven, cake, degree, time, next);
 
 		return true;
 		});
@@ -478,7 +485,14 @@ int main() {
 
 	deco_cream->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
 
-		if (deco_cream_state == 0) {
+		if (stage == 7) {
+			result->show();
+			end->show();
+			if (chili_state == 1) result->setImage("Images/결과_매움.png");
+			else if (sugar_state == 0 || butter_state == 0) result->setImage("Images/결과_부족.png");
+			else if (salt_state == 1 || flour_choice == -1) result->setImage("Images/결과_이상.png");
+		}
+		else if (deco_cream_state == 0) {
 			deco_cream_->show();
 			deco_cream_state = 1;
 		}
@@ -539,7 +553,14 @@ int main() {
 
 	deco_candle->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
 
-		if (deco_candle_state == 0) {
+		if (stage == 7) {
+			result->show();
+			end->show();
+			if (chili_state == 1) result->setImage("Images/결과_매움.png");
+			else if (sugar_state == 0 || butter_state == 0) result->setImage("Images/결과_부족.png");
+			else if (salt_state == 1 || flour_choice == -1) result->setImage("Images/결과_이상.png");
+		}
+		else if (deco_candle_state == 0) {
 			deco_candle_->show();
 			deco_candle_state = 1;
 		}
@@ -554,7 +575,14 @@ int main() {
 
 	deco_choco->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
 
-		if (deco_choco_state == 0) {
+		if (stage == 7) {
+			result->show();
+			end->show();
+			if (chili_state == 1) result->setImage("Images/결과_매움.png");
+			else if (sugar_state == 0 || butter_state == 0) result->setImage("Images/결과_부족.png");
+			else if (salt_state == 1 || flour_choice == -1) result->setImage("Images/결과_이상.png");
+		}
+		else if (deco_choco_state == 0) {
 			deco_choco_->show();
 			deco_choco_state = 1;
 		}
@@ -569,8 +597,24 @@ int main() {
 
 	//***********************************거실-stage7:평가***********************************//
 
+	cake->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
+		if (stage == 7) {
+			result->show();
+			end->show();
+			if (chili_state == 1) result->setImage("Images/결과_매움.png");
+			else if (sugar_state == 0 || butter_state == 0) result->setImage("Images/결과_부족.png");
+			else if (salt_state == 1 || flour_choice == -1) result->setImage("Images/결과_이상.png");
+		}
+		
 
+		return true;
+		});
 
+	end->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
+		endGame();
+
+		return true;
+		});
 
 	//***********************************공통***********************************//
 	done->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
@@ -592,19 +636,17 @@ int main() {
 		else if (stage == 5) {
 			cream->hide();
 			cream_choco->hide();
-			cream_berry->hide();
-			prev->show();
 			next->show();
 
 		}
 		else if (stage == 6) {
 			deco_cream->hide();
-			deco_strawberry->hide();
 			deco_blueberry->hide();
 			deco_cherry->hide();
 			deco_candle->hide();
 			deco_choco->hide();
-			prev->show();
+			deco_strawberry->hide();
+			next->setImage("Images/결과 확인.png");
 			next->show();
 		}
 		done->hide();
@@ -633,7 +675,6 @@ int main() {
 			butter_->hide();
 		}
 		whisk->hide();
-		prev->show();
 		next->show();
 
 		return true;
@@ -641,35 +682,7 @@ int main() {
 
 		});
 
-	prev->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
-		if (stage == 1) {
-			egg1->show();
-			egg2->show();
-			egg3->show();
-			egg_basket->show();
-			egg_broken->hide();
-			egg_beaten->hide();
-			whisk->hide();
-			egg_count = 0;
-			stage = 1;
 
-		}
-		else if (stage == 2) {
-			egg_beaten->show();
-			flour->hide();
-			flour_dough->hide();
-			flour1->show();
-			flour2->show();
-			stage = 2;
-		}
-		else if (stage == 3) {
-
-		}
-		prev->hide();
-		next->hide();
-
-		return true;
-		});
 
 	next->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
 
@@ -697,7 +710,6 @@ int main() {
 		else if (stage == 3) {
 			showMessage("적절한 온도와 시간로 케이크를 구워보자!");
 			oven->show();
-			cake_pre->show();
 			degree->show();
 			time->show();
 			bowl->hide();
@@ -722,17 +734,20 @@ int main() {
 			deco_candle->show();
 			deco_choco->show();
 			done->show();
-			done->locate(scene2, 520, 50);
+			done->locate(scene2, 500, 20);
 			stage = 6;
 		}
 		else if (stage == 6) {
-
+			showMessage("케이크를 눌러 결과를 확인하세요!");
+			stage = 7;
 		}
-		prev->hide();
 		next->hide();
 
 		return true;
 		});
+
+
+
 
 	startGame(scene1);
 }
